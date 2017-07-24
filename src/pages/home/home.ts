19df599f580ed8564169525908b36346c39d311f
Alex Lonsky <lonik_777@mail.ru>
2017-07-25 02:05:39 +0300

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,55 +7,70 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
     public swipe: number = 0;
-    public align:any = 'right';
-    public items = [
-        {
-            title: 'Milk 1l',
-            status: false
-        }
-        , {
-            title: 'Eggs Medium 12 pack',
-            status: true
-        },
-        {
-            title: 'Fresh Basil',
-            status: false
-        },
-        {
-            title: 'Wholegrain Bread 1 pkg',
-            status: true
-        },
-        {
-            title: 'Ground Coffee 200g',
-            status: true
-        },
-        {
-            title: 'Red Wine',
-            status: false
-        },
-        {
-            title: 'Mozzarella Cheese 150g',
-            status: false
-        },
-        {
-            title: 'Orange Juice 1l',
-            status: true
-        },
-        {
-            title: 'Tomatoes',
-            status: false
-        }
-    ];
+    public align: any = 'right';
+    public items:any = [];
+    public basket:any = [];
+    public view:boolean = false;
 
     constructor(public navCtrl: NavController) {
+        this.initItems()
+    }
+    initItems(){
+        this.items = [
+            {
+                title: 'Milk 1l',
+                status: false
+            }
+            , {
+                title: 'Eggs Medium 12 pack',
+                status: true
+            },
+            {
+                title: 'Fresh Basil',
+                status: false
+            },
+            {
+                title: 'Wholegrain Bread 1 pkg',
+                status: true
+            },
+            {
+                title: 'Ground Coffee 200g',
+                status: true
+            },
+            {
+                title: 'Red Wine',
+                status: false
+            },
+            {
+                title: 'Mozzarella Cheese 150g',
+                status: false
+            },
+            {
+                title: 'Orange Juice 1l',
+                status: true
+            },
+            {
+                title: 'Tomatoes',
+                status: false
+            }
+        ];
 
     }
 
     swipeEvent(item) {
-        console.log(item)
         item.status = item.status ? false : true;
-        console.log(item)
-        console.log(this.items)
     }
 
+    showBasket() {
+        this.view = true;
+        this.basket = this.items.filter((item) => {
+            return (item.status == true);
+        })
+
+    }
+    showAllItems(){
+        this.view = false;
+        this.basket = [];
+
+    }
 }
